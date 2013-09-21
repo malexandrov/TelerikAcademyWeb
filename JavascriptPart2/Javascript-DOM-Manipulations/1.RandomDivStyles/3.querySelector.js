0c3660db1d2dd0ea42a9eb29b7f809210dbff33e
malexandrov <malexandrov.kp@gmail.com>
2013-09-21 16:50:56 +0300

    Added JavaScript Part 2 exercises

---
+++ JavascriptPart2/Javascript-DOM-Manipulations/1.RandomDivStyles/3.querySelector.js
@@ -0,0 +1,18 @@
+﻿if (!document.querySelector || !document.querySelectorAll) {
+        document.querySelectorAll = function (selector) {
+            switch (selector.charAt(0)) {
+                case "#": return document.getElementById(selector.substring(1, selector.length));
+                case ".": return document.getElementsByClassName(selector.substring(1, selector.length));
+                default: return document.getElementsByTagName(selector);
+            }
+        }
+     
+        document.querySelector = function (selector) {
+            switch (selector.charAt(0)) {
+                case "#": return document.getElementById(selector.substring(1, selector.length))[0];
+                case ".": return document.getElementsByClassName(selector.substring(1, selector.length))[0];
+                default: return document.getElementsByTagName(selector)[0];
+            }
+        }
+    }
+
